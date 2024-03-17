19634c5dd1b3b844587955c5e762165cea35f86c
subham gupta <0subhamgupta1@gmail.com>
2024-03-17 11:24:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+mongoose.connect(process.env.DB_URL).then(() => {
+  console.log("db connected");
+});
+
+app.listen(8000, (err) => {
+  console.log("server running at 8000");
+  if (err) console.log("err", err);
+});
---
+++ readme.md
@@ -0,0 +1,6 @@
+signIn
+creating project
+choosing m0 free server
+security> username password
+security> networkAccess> IP address 0.0.0.0/0
+create db and its collections (user books orders etc)
